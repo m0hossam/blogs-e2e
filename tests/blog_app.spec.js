087cbe662335e0219d4337b6cee53418eb4b1975
m0hossam <mohossam1326@gmail.com>
2025-11-11 01:09:21 +0200

    Finish 5.17

---
+++ tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Login to the app')).toBeVisible()
+    await expect(page.getByLabel('Username')).toBeVisible()
+    await expect(page.getByLabel('Password')).toBeVisible()
+    await expect(page.getByRole('button')).toBeVisible()
+  })
+})
