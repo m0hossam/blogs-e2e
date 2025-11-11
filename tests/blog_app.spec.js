275f90a19f7e90f12c37a33ce4328393a7a0e334
m0hossam <mohossam1326@gmail.com>
2025-11-11 02:36:24 +0200

    Finish 5.22

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -7,17 +7,33 @@ const admin = {
   name: 'Mohamed Hossam'
 }
 
+const otherUser = {
+  username: 'bond007',
+  password: 'bond007',
+  name: 'James Bond'
+}
+
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('/api/testing/reset') // Clear DB
-    await request.post('api/users', { // Create a default admin user
+    // Clear DB
+    await request.post('/api/testing/reset')
+    // Create a default admin user
+    await request.post('api/users', {
       data: {
         username: admin.username,
         password: admin.password,
         name: admin.name
       }
     })
-
+    // Create another user for testing
+    await request.post('api/users', {
+      data: {
+        username: otherUser.username,
+        password: otherUser.password,
+        name: otherUser.name
+      }
+    })
+    // Launch web app
     await page.goto('/')
   })
 
@@ -64,12 +80,26 @@ describe('Blog app', () => {
         await expect(page.getByText('Likes: 1')).toBeVisible()
       })
 
-      test.only('blog can be deleted by its user', async ({ page }) => {
+      test('blog can be deleted by its user', async ({ page }) => {
         await page.getByRole('button', { name: 'Show details' }).click()
         page.on('dialog', dialog => dialog.accept()) // Register a dialog handler to accept the delete confirmation dialog
         await page.getByRole('button', { name: 'Remove' }).click()
         await expect(page.getByText('Blog removed')).toBeVisible()
         await expect(page.getByText('Chronicles of the Wild West - Clint Eastwood')).not.toBeVisible()
+      })
+
+      test.only('only the user who added the blog sees its delete button', async ({ page }) => {
+        // admin can see the blog since he's the creator
+        await page.getByRole('button', { name: 'Show details' }).click()
+        await expect(page.getByRole('button', { name: 'Remove' })).toBeVisible()
+
+        // admin logs out, other user logs in
+        await page.getByRole('button', { name: 'Logout' }).click()
+        await loginWith(page, otherUser.username, otherUser.password)
+
+        // other use CANNOT see the blog since he's NOT the creator
+        await page.getByRole('button', { name: 'Show details' }).click()
+        await expect(page.getByRole('button', { name: 'Remove' })).not.toBeVisible()
       })
     })
   })
